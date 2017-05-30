1f89178164bab882e34261053b4951b9e7ed6f81
Florin-Lazar <florin-lazar@live.com>
2017-05-30 16:21:18 +0300

    N-Tier architecture - web presentation

---
+++ Courses/Apps/Architecture/Intro/NTierBasic/Presentation.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLogic;
+using DataAccess.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.Extensions;
+using Models.Paging;
+using Models.Sorting;
+using System;
+
+namespace Presentation.WebUI.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly IPersonRepository personRepository;
+
+    public HomeController(IPersonRepository personRepository)
+    {
+      if (personRepository == null)
+      {
+        throw new ArgumentNullException($"{nameof(personRepository)}");
+      }
+
+      this.personRepository = personRepository;
+    }
+
+    public IActionResult Index()
+    {
+      return this.List();
+    }
+
+    public IActionResult List(int pageIndex = 0, int pageSize = 20, PersonSortCriteria criteria = PersonSortCriteria.ById, SortDirection sortDirection = SortDirection.Ascending)
+    {
+      var personBO = new PersonBusinessObject(this.personRepository);
+
+      var data = personBO.GetPersonsPaged(pageIndex, pageSize, criteria, sortDirection);
+
+      if (pageIndex > data.GetLastPageIndex())
+      {
+        pageIndex = data.GetLastPageIndex();
+
+        data = personBO.GetPersonsPaged(pageIndex, pageSize, criteria, sortDirection);
+      }
+
+      return View("Index", data);
+    }
+
+    public IActionResult Error()
+    {
+      return View();
+    }
+  }
+}
---
+++ Courses/Apps/LINQ/LINQExercises/LINQExercises/Results.paged-collection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQExercises
+{
+  public partial class Results
+  {
+    public class PagedCollection<T>
+    {
+      public PagedCollection(
+        IEnumerable<T> itemsPerPage,
+        int pageSize,
+        int pageIndex,
+        int totalElementsCount)
+      {
+        if (pageSize < 0)
+        {
+          throw new ArgumentException($"Page size ('{pageSize}') must be a positive number!");
+        }
+
+        if (pageIndex < 0)
+        {
+          throw new ArgumentException($"Page index ('{pageIndex}') must be a positive number!");
+        }
+
+        if (totalElementsCount < 0)
+        {
+          throw new ArgumentException($"Total elements count ('{totalElementsCount}') must be a positive number!");
+        }
+
+        if (itemsPerPage == null)
+        {
+          itemsPerPage = Enumerable.Empty<T>();
+        }
+
+        this.Items = itemsPerPage;
+        this.PageSize = pageSize;
+        this.PageIndex = pageIndex;
+        this.TotalElementsCount = totalElementsCount;
+      }
+
+      public IEnumerable<T> Items { get; private set; }
+
+      public int PageSize { get; private set; }
+
+      public int PageIndex { get; private set; }
+
+      public int TotalElementsCount { get; private set; }
+    }
+  }
+}
